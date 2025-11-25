fbc92cd92824b4a43c02586de65cb764f94980ea
oriss-dev <orissolorzano@gmail.com>
2025-11-24 21:30:00 -0300

    correciones de dependencias

--- usuario/src/main/java/com/micro/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/com/micro/usuario/controller/UsuarioController.java
@@ -4,14 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("api/v1")
 public class UsuarioController {
 
-    @PostMapping("home/admin")
+    @GetMapping("/admin/home")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> adminHome() {
         return ResponseEntity.ok("Bienvenido Admin");
